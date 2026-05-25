f9bbc2b6a096c8ff172eaeb752c20f25b750ad11
Sathwik-Devireddy <165791347+Sathwik-Devireddy@users.noreply.github.com>
2026-05-25 18:04:59 +0530

    Refactor openRouter.js to use OpenRouter SDK

--- server/config/openRouter.js
+++ server/config/openRouter.js
@@ -1,8 +1,8 @@
-const openRouterUrl =
-  "https://openrouter.ai/api/v1/chat/completions";
+import { OpenRouter } from "@openrouter/sdk";
+const openRouterUrl = "https://openrouter.ai/api/v1/chat/completions";
+const openRouterApiKey = process.env.OPENROUTER_API_KEY;
 
 const model = "deepseek/deepseek-chat-v3-0324";
-
 export const generateResponse = async (prompt) => {
   const res = await fetch(openRouterUrl, {
     method: "POST",
@@ -13,32 +13,26 @@ export const generateResponse = async (prompt) => {
     },
 
     body: JSON.stringify({
-      model,
+      model: model,
 
       messages: [
         {
           role: "system",
-
           content: "You must return ONLY valid raw JSON.",
         },
 
         {
           role: "user",
-
           content: prompt,
         },
       ],
-
       temperature: 0.2,
     }),
   });
-
   if (!res.ok) {
     const err = await res.text();
-
     throw new Error(`OpenRouter API error: ${err}`);
   }
-
   const data = await res.json();
 
   return data.choices[0].message.content;
