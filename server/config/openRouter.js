0974badb54ecb612aa0ed04bd6980d0706f332a0
Sathwik-Devireddy <165791347+Sathwik-Devireddy@users.noreply.github.com>
2026-05-25 19:48:48 +0530

    Update openRouter.js

--- server/config/openRouter.js
+++ server/config/openRouter.js
@@ -2,6 +2,7 @@ import { OpenRouter } from "@openrouter/sdk";
 const openRouterUrl = "https://openrouter.ai/api/v1/chat/completions";
 const openRouterApiKey = process.env.OPENROUTER_API_KEY;
 
+
 const model = "deepseek/deepseek-chat-v3-0324";
 export const generateResponse = async (prompt) => {
   const res = await fetch(openRouterUrl, {
