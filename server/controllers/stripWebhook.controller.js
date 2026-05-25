2dee1d6addc714b4eeead22a4cacab90b9019302
Sathwik-Devireddy <165791347+Sathwik-Devireddy@users.noreply.github.com>
2026-05-25 13:19:56 +0530

    Update stripWebhook.controller.js

--- server/controllers/stripWebhook.controller.js
+++ server/controllers/stripWebhook.controller.js
@@ -6,29 +6,48 @@ dotenv.config();
 
 export const stripeWebhook = async (req, res) => {
   const sig = req.headers["stripe-signature"];
+
   let event;
+
   try {
-    event = await stripe.webhooks.constructEvent(
-      // Verify the webhook signature
+    event = stripe.webhooks.constructEvent(
       req.body,
       sig,
-      process.env.STRIPE_WEBHOOK_SECRET,
+      process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (error) {
     console.log(error.message);
+
     return res.status(400).send(`Webhook Error: ${error.message}`);
   }
-  if (event.type == "checkout.session.completed") {
+
+  if (event.type === "checkout.session.completed") {
     const session = event.data.object;
+
+    console.log("Payment Successful");
+    console.log(session.metadata);
+
     const userId = session.metadata.userId;
     const credits = Number(session.metadata.credits);
     const plan = session.metadata.plan;
+
     const user = await User.findById(userId);
+
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({
+        message: "User not found",
+      });
     }
+
     user.credits += credits;
     user.plan = plan;
+
     await user.save();
+
+    console.log("Credits Updated");
   }
+
+  return res.status(200).json({
+    received: true,
+  });
 };
